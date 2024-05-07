1cdb4261bf6c029bc6c74eae5821880b084b959e
ThiagoHMarino <thiagomarino@MacBook-Air-de-Thiago.local>
2024-05-07 17:29:47 -0300

    first commit

---
+++ list.c
@@ -0,0 +1,5 @@
+//
+// Created by Thiago Marino on 07/05/2024.
+//
+
+#include "list.h"
---
+++ list.h
@@ -0,0 +1,8 @@
+//
+// Created by Thiago Marino on 07/05/2024.
+//
+
+#ifndef EJ10_LISTAS_LIST_H
+#define EJ10_LISTAS_LIST_H
+
+#endif //EJ10_LISTAS_LIST_H
